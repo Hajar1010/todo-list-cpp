305258573b3ae51a5ad6ae473c51980c4ad76bd4
Hajar <hajaressalihi9@gmail.com>
2026-04-22 21:38:46 +0100

    Add Task class definition in Task.h
    This header file defines the abstract Task class with its attributes and important functions.

---
+++ include/Task.h
@@ -0,0 +1,16 @@
+// This is the MAIN class of the project.
+//
+// A Task represents one thing the user wants to do.
+//
+// It contains:
+// - title (name of task)
+// - description
+// - priority
+// - status
+//
+// It also defines important functions:
+// - display() → show task info
+// - computeUrgency() → calculate how urgent it is (IMPORTANT for polymorphism)
+//
+// This class is ABSTRACT because computeUrgency() is not implemented here.
+// Other classes will inherit from it.
