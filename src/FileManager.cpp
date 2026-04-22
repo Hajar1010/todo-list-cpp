e1795393ad33c4861bbde504ba37cdc1cc2decad
Hajar <hajaressalihi9@gmail.com>
2026-04-22 21:40:34 +0100

    Add FileManager.cpp for file operations
    This file implements file operations for task data storage.

---
+++ src/FileManager.cpp
@@ -0,0 +1,9 @@
+// This file implements file operations.
+//
+// It writes task data into a file.
+//
+// Example:
+// If the user creates tasks,
+// they will be stored in "data/tasks.txt".
+//
+// Later, we can also add loading functionality.
