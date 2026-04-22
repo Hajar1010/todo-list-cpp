6df7470f2289506e948d93f19cb108751695bcb5
Hajar <hajaressalihi9@gmail.com>
2026-04-22 21:39:45 +0100

    Add TaskManager class implementation
    Initial implementation of the TaskManager class with comments.

---
+++ src/TaskManager.cpp
@@ -0,0 +1,9 @@
+// This file implements the TaskManager class.
+//
+// It defines:
+// - how tasks are added to the list
+// - how tasks are displayed
+// - how tasks are deleted
+//
+// It works with pointers to Task objects.
+// This allows us to use polymorphism later.
