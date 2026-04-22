f5af6a66f60b278856fb54a25395344026caeea9
Hajar <hajaressalihi9@gmail.com>
2026-04-22 21:38:30 +0100

    Add Status.h to define task statuses
    This file defines the STATUS of a task, including TODO, IN_PROGRESS, and DONE states to help track task progress.

---
+++ include/Status.h
@@ -0,0 +1,8 @@
+// This file defines the STATUS of a task.
+//
+// A task can be:
+// - TODO (not started)
+// - IN_PROGRESS (currently working on it)
+// - DONE (finished)
+//
+// This helps track progress of tasks.
