0a55dbcd35adf1edeac41920bfe12289f6fee137
Hajar <hajaressalihi9@gmail.com>
2026-04-22 21:39:03 +0100

    Implement Task class with constructor and methods
    Add implementation details for the Task class.

---
+++ src/Task.cpp
@@ -0,0 +1,9 @@
+// This file contains the IMPLEMENTATION of the Task class.
+//
+// It defines:
+// - how a task is created (constructor)
+// - how a task is displayed
+// - getters (getTitle, getPriority, etc.)
+//
+// It also defines operator <<
+// so we can print a task easily using cout.
