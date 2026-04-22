ea612bd6ca850c9bb8055aedc2f34003adc2a84d
Hajar <hajaressalihi9@gmail.com>
2026-04-22 21:40:10 +0100

    Create FileManager.h for saving tasks
    Add FileManager class for task data management

---
+++ include/FileManager.h
@@ -0,0 +1,8 @@
+// This class is responsible for saving data.
+//
+// It will:
+// - save tasks into a file (tasks.txt)
+// - (later) load tasks from the file
+//
+// This allows the program to remember tasks
+// even after it is closed.
