fbba7b0df68faf4af7f1afb76d907451c8d2ed1d
Hajar <hajaressalihi9@gmail.com>
2026-04-26 11:41:22 +0100

    Refactor comments and add include guards in TaskManager

--- include/TaskManager.h
+++ include/TaskManager.h
@@ -9,3 +9,21 @@
 //
 // It does NOT care about how tasks work internally.
 // It just manages them.
+
+
+
+#pragma once
+#include <vector>
+#include "Task.h"
+
+class TaskManager {
+private:
+    std::vector<Task*> tasks;
+
+public:
+    void addTask(Task* task);
+    void displayTasks() const;
+    void removeTask(int index);
+
+    std::vector<Task*>& getTasks();
+};
