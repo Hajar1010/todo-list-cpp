58e1121f04267d44fa936c914c94afda915a1f33
Hajar <hajaressalihi9@gmail.com>
2026-04-26 11:40:50 +0100

    Refactor comments in Task.h for clarity

--- include/Task.h
+++ include/Task.h
@@ -14,3 +14,30 @@
 //
 // This class is ABSTRACT because computeUrgency() is not implemented here.
 // Other classes will inherit from it.
+
+#pragma once
+#include <string>
+#include <iostream>
+#include "Priority.h"
+#include "Status.h"
+
+class Task {
+protected:
+    std::string title;
+    std::string description;
+    Priority priority;
+    Status status;
+
+public:
+    Task(std::string t, std::string d, Priority p, Status s);
+
+    virtual void display() const;
+    virtual int computeUrgency() const = 0;
+    std::string getTitle() const;
+    Priority getPriority() const;
+    Status getStatus() const;
+
+    virtual ~Task() {}
+
+    friend std::ostream& operator<<(std::ostream& os, const Task& task);
+};
