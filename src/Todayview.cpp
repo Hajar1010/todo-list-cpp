f6f7b6ce8017c380e52e2feee99adb94da532683
Hajar <hajaressalihi9@gmail.com>
2026-05-09 17:57:25 +0100

    fixed the remove , deleted some fct

--- src/Todayview.cpp
+++ src/Todayview.cpp
@@ -2,15 +2,22 @@
 
 std::vector<Task*> TodayView::getTodayTasks(const std::vector<Task*>& tasks) {
     std::vector<Task*> result;
+
     for (Task* t : tasks) {
-        if (t->getStatus() == Status::DONE) continue;
-        if (!t->getDeadline()) {
-            if (t->getStatus() == Status::IN_PROGRESS)
-                result.push_back(t);
-        } else {
-            if (t->getDeadline()->daysLeft() <= 1)
-                result.push_back(t);
+
+        // skip completed tasks
+        if (t->getStatus() == Status::DONE)
+            continue;
+
+        // must have a deadline
+        if (!t->getDeadline())
+            continue;
+
+        // ONLY tasks due today
+        if (t->getDeadline()->daysLeft() == 0) {
+            result.push_back(t);
         }
     }
+
     return result;
 }
